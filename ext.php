0f3bb87f202f6befbd32479e84a088e6c22eb2bd
Richard McGirr <rmcgirr83@gmail.com>
2020-08-08 16:08:16 -0500

    Check for mbstring on install, toss appropriate error.

--- ext.php
+++ ext.php
@@ -9,13 +9,20 @@
 
 namespace rmcgirr83\sfpo;
 
+use phpbb\extension\base;
 /**
 * Extension class for custom enable/disable/purge actions
 */
-class ext extends \phpbb\extension\base
+class ext extends base
 {
+	/** @var Extension name */
+	const EXT_NAME = 'sfpo';
+
+	/** @var phpBB check version */
+	const PHPBB_VERSION = '3.2.0';
+
 	/**
-	 * Enable extension if phpBB version requirement is met
+	 * Enable extension if phpBB and mbstring version requirement is met
 	 *
 	 * @return bool
 	 * @access public
@@ -23,6 +30,20 @@ class ext extends \phpbb\extension\base
 	public function is_enableable()
 	{
 		$config = $this->container->get('config');
-		return phpbb_version_compare($config['version'], '3.2.0', '>=');
+
+		$language = $this->container->get('language');
+		$language->add_lang('ext_enable_error', 'rmcgirr83/'. self::EXT_NAME);
+
+		if (!extension_loaded('mbstring'))
+		{
+			trigger_error($language->lang('EXT_MBSTRING_ERROR'), E_USER_WARNING);
+		}
+
+		if (!(phpbb_version_compare($config['version'], self::PHPBB_VERSION, '>=')))
+		{
+			trigger_error($language->lang('EXT_PHPBB_ERROR', self::PHPBB_VERSION, $config['version']), E_USER_WARNING);
+		}
+
+		return true;
 	}
 }
